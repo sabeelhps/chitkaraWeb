ae2206270dd89aed7b2d684ea1f3f2e4319a5459
Sabeel Khan <sabeelhps@gmail.com>
2021-03-15 14:11:19 +0530

    Lecture-13

--- Lecture-13/TodoApp/assets/js/app.js
+++ Lecture-13/TodoApp/assets/js/app.js
@@ -1,11 +1,11 @@
 
 // Marking li as completed
-$('ul').on('click', 'li', function () {
+$('#container ul').on('click', 'li', function () {
     $(this).toggleClass('completed');
 })
 
 // removing li items from our todo
-$('ul').on('click', 'span', function (event) {
+$('#container ul').on('click', 'span', function (event) {
     $(this).parent().fadeOut(function () {
         // console.log("FadeOut Completed");
         $(this).remove();
@@ -14,13 +14,12 @@ $('ul').on('click', 'span', function (event) {
     event.stopPropagation();  //to prevent event bubbling
 })
 
-
-
 // Adding new todo 
 $('input[type="text"]').keypress(function (event) {
     if (event.which === 13) {
+        // console.log(event);
         let newTodoText = $(this).val();
-        $('#container ul').append(`<li><span><i class="fas fa-trash-alt"></i></span> ${newTodoText}</li>`)
+        $('#container ul').append(`<li><span><i class="fas fa-trash-alt"></i></span> ${newTodoText}</li>`);
         $(this).val("");
     } 
 })
