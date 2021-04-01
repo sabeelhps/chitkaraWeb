b4ff30e30389cc34a26017d5d509de5a538acbee
Sabeel Khan <sabeelhps@gmail.com>
2021-04-01 14:21:00 +0530

    Lecture-20

--- Lecture-20/MongooseWithExpress/index.js
+++ Lecture-20/MongooseWithExpress/index.js
@@ -3,9 +3,10 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const seedDB = require('./seedDB');
+const methodOverride = require('method-override');
 
 
-mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {
         console.log("Connection Open");
     })
@@ -17,6 +18,8 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
+mongoose.set('useFindAndModify', false);
 
 
 // seedDB();
@@ -60,6 +63,35 @@ app.get('/products/:id', async(req, res) => {
     const product = await Product.findById(req.params.id);
 
     res.render('products/show',{product:product})
+})
+
+// Get form for edit product
+
+app.get('/products/:id/edit', async(req, res) => {
+    
+    const foundProduct = await Product.findById(req.params.id);
+
+    res.render('products/edit',{product:foundProduct})
+})
+
+
+// Making a PATCH Request
+app.patch('/products/:id', async(req, res) => {
+
+    // const foundProduct = await Product.findById(req.params.id);
+
+    const updatedProduct = await Product.findOneAndUpdate({ _id: req.params.id },req.body);
+   
+    res.redirect('/products')
+})
+
+
+// Delete particular product
+app.delete('/products/:id', async(req, res) => {
+    
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+
+    res.redirect('/products');
 })
 
 app.listen(3000, () => {
